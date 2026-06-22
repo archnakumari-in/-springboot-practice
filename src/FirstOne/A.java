07c0fe226063822182a2886c2f07f12b27613912
Archna Kumari <archna.kumari@uneecops.in>
2026-06-22 20:26:50 +0530

    update file

--- src/FirstOne/A.java
+++ src/FirstOne/A.java
@@ -1,8 +1,15 @@
 package FirstOne;
 
 public class A {
+	
+	public void add(int a, int b) {
+		System.out.println(a + b);
+	}
   public static void main(String[] args) {
 	System.out.println("Hello, World!");
+	
+	A obj = new A();
+	obj.add(5, 10);
   }
 
 }
